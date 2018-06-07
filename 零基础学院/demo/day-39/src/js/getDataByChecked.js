9ba0232abf5680f74fddffd3c6ad341e9a7121d8
xiaojianbu <xufeng-2016@qq.com>
2018-06-07 19:33:32 +0800

    build:(前端学院Day-39-41 项目初始化)

---
+++ 零基础学院/demo/day-39/src/js/getDataByChecked.js
@@ -0,0 +1,61 @@
+import {
+  sourceData
+} from './ife31data.js'
+
+/**
+ * 根据checkbox获取数据
+ * @returns {Array of Object}
+ */
+export function getDataByChecked() {
+  let regionChecked = []
+  let productChecked = []
+
+  let regionElements = document.querySelectorAll(
+    'input[name="region-radio-wrapper-select"]:checked'
+  )
+  let productElements = document.querySelectorAll(
+    'input[name="product-radio-wrapper-select"]:checked'
+  )
+  for (let i = 0; i < regionElements.length; i++) {
+    regionChecked.push(regionElements[i].value)
+  }
+  for (let i = 0; i < productElements.length; i++) {
+    productChecked.push(productElements[i].value)
+  }
+
+  let data = []
+
+  sourceData.forEach(item => {
+    if (regionChecked.length && productChecked.length) {
+      if (regionChecked.includes(item.region) &&
+        productChecked.includes(item.product)) {
+        // 从localStorage中获取数据
+        if (localStorage.getItem(`${item.product}-${item.region}`)) {
+          data.push(JSON.parse(localStorage.getItem(`${item.product}-${item.region}`)))
+        } else {
+          data.push(item)
+        }
+      }
+    } else if (regionChecked.length && !productChecked.length) {
+      if (regionChecked.includes(item.region)) {
+        // 从localStorage中获取数据
+        if (localStorage.getItem(`${item.product}-${item.region}`)) {
+          data.push(JSON.parse(localStorage.getItem(`${item.product}-${item.region}`)))
+        } else {
+          data.push(item)
+        }
+      }
+    } else if (!regionChecked.length && productChecked.length) {
+      if (productChecked.includes(item.product)) {
+        // 从localStorage中获取数据
+        if (localStorage.getItem(`${item.product}-${item.region}`)) {
+          data.push(JSON.parse(localStorage.getItem(`${item.product}-${item.region}`)))
+        } else {
+          data.push(item)
+        }
+      }
+    }
+  })
+
+  return data
+}
