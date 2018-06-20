58171c2b058bd63d8d2e1448382af513b9986913
xiaojianbu <xufeng-2016@qq.com>
2018-06-20 16:23:31 +0800

    build(前端学院Day-47): 添加url-loader

--- 零基础学院/demo/day-47/config/webpack.common.js
+++ 零基础学院/demo/day-47/config/webpack.common.js
@@ -27,6 +27,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader'
       }
     ]
   }
